5d5d76d5ca0b7b616e9d310ddd911d0bea608774
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-16 02:00:16 +0530

    Delete BrowserTab.cs

--- BrowserTab.cs
+++
@@ -1,7 +0,0 @@
-namespace TradingBrowser;
-public class BrowserTab
-{
-    public string Title { get; set; } = "New Tab";
-    public string Url { get; set; } = "https://www.tradingview.com/";
-    public double ZoomFactor { get; set; } = 1.0;
-}
