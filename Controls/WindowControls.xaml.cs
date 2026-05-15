c2cb42a1dd3a50d7af897fbd79d22c52cb3662fc
Rajendertyagi <86180775+Rajendertyagi@users.noreply.github.com>
2026-05-16 02:22:46 +0530

    Add files via upload

--- Controls/WindowControls.xaml.cs
+++ Controls/WindowControls.xaml.cs
@@ -1,16 +1,13 @@
 using System.Windows;
 using System.Windows.Controls;
-
 namespace TradingBrowser.Controls;
-
 public partial class WindowControls : UserControl
 {
     public WindowControls()
     {
         InitializeComponent();
         MinimizeButton.Click += (_, _) => Window.GetWindow(this)!.WindowState = WindowState.Minimized;
-        MaximizeButton.Click += (_, _) =>
-        {
+        MaximizeButton.Click += (_, _) => {
             var w = Window.GetWindow(this)!;
             w.WindowState = w.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
         };
